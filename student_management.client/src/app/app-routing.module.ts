981ec42257fc0b137290aea8d1b9d9c77f01cafa
Pratham <prathamnaveen.m@gmail.com>
2024-01-02 12:50:03 +0530

    latest

--- student_management.client/src/app/app-routing.module.ts
+++ student_management.client/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { StudentListComponent } from './components/student-list/student-list.component';
 import { StudentDetailComponent } from './components/student-detail/student-detail.component';
 import { StudentFormComponent } from './components/student-form/student-form.component';
+import { SchoolListComponent } from './components/school-list/school-list.component';
 
 const routes: Routes = [
   { path: 'students', component: StudentListComponent },
@@ -10,6 +11,7 @@ const routes: Routes = [
   { path: 'add-student', component: StudentFormComponent },
   { path: 'edit-student/:id', component: StudentFormComponent },
   { path: '', redirectTo: '/students', pathMatch: 'full' },
+  { path: 'school-list', component: SchoolListComponent },
 ];
 
 @NgModule({
--- student_management.client/src/app/app.module.ts
+++ student_management.client/src/app/app.module.ts
@@ -8,6 +8,8 @@ import { AppComponent } from './app.component';
 import { StudentListComponent } from './components/student-list/student-list.component';
 import { StudentDetailComponent } from './components/student-detail/student-detail.component';
 import { StudentFormComponent } from './components/student-form/student-form.component';
+import { SchoolListComponent } from './components/school-list/school-list.component';
+import { SchoolFormComponent } from './components/school-form/school-form.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +17,8 @@ import { StudentFormComponent } from './components/student-form/student-form.com
     StudentListComponent,
     StudentDetailComponent,
     StudentFormComponent,
+    SchoolListComponent,
+    SchoolFormComponent,
   ],
   imports: [BrowserModule, HttpClientModule, FormsModule, AppRoutingModule],
   providers: [],
